9cb8a453e9b72c7fda1134de44560e3f2c34a936
jasonaix <zr@nurok.ai>
2025-12-11 19:28:54 +0800

    fix workflow build error

--- scripts/generate_api_sdk.sh
+++ scripts/generate_api_sdk.sh
@@ -8,10 +8,6 @@ openapi-generator-cli generate \
   -o ./api/crawl4ai-client \
   -t ./templates
   
-# find ./api/crawl4ai-client -type f -name "*.rs" -exec sed -i '' 's/use crate::models;/use crate::generated::models;/g' {} \;
-# find ./api/crawl4ai-client -type f -name "*.rs" -exec sed -i '' 's/crate::apis/crate::generated::apis/g' {} \;
-# find ./api/crawl4ai-client -type f -name "*.rs" -exec sed -i '' 's/use crate::{apis::ResponseContent, models};/use crate::generated::{apis::ResponseContent, models};/g' {} \;
-
 rm -rf ./src/generated/apis
 rm -rf ./src/generated/models
 cp -r ./api/crawl4ai-client/src/apis ./src/generated/apis
